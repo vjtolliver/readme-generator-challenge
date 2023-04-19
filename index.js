f5471e29b80383705586ee02eb353ef13ed09aab
vjtolliver <vj.tolliver@gmail.com>
2023-04-18 20:41:54 -0700

    added question prompts

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [{
+const questions = [
+    {
     type: 'input',
+    message: 'What is the name of your repository?',
     name: 'title',
-    message: 'What is the name of your repository?'
 },
 {
     type: 'input',
+    message: 'What is your GitHub username?',
     name: 'username',
-    message: 'What is your GitHub username',
 },
 {
     type: 'input',
@@ -21,42 +22,65 @@ const questions = [{
 {
     typut: 'input',
     name: 'installation',
-    message:
+    message: 'Please enter the installation instuctions for you project.',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Please enter your usage information.',
+}, 
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Please enter your contribution guidelines.',
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'Please enter your test instructions.',
+},
+{
+    type: 'input',
+    name: 'contact',
+    message: 'Please enter how to reach you and your contact information.',
 }
-];
+]
+
 
 // TODO: Create a function to write README file
 inquirer.prompt (questions)
 .then((response) => {
-    console.log(response);
+    //console.log(response);
     writeToFile("READMES.md", response)
-})
+}
+)
 
 function writeToFile(fileName, data) {
     const finalPage = `# ${data.title}
-    ## Description
-    ${data.description}
+     ## Description
+     ${data.description}
     
-    ##Table of Content
+     ##Table of Content
     
-    ##Installation
+     ##Installation
     
-    ##Usage
+     ##Usage
     
-    ##Contributing
+     ##Contributing
     
-    ##Tests
+     ##Tests
     
-    ##License
+     ##License
     
-    ##Questions
-    GitHub: ${data-username}
-   ${data-contactinfo} `
-}
+     ##Questions
+     GitHub: ${data-username}
+    ${data-contactinfo} `
+// }
 
 fs.writeFile(fileName, finalPage, function(error) {
-
+error ? console.log("error") : console.log("success!");
 })
+};
 
 // TODO: Create a function to initialize app
 function init() {}
