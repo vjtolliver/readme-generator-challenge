9e1006c98b3b262c230d43ec5653fdba8c5f28a7
vjtolliver <vj.tolliver@gmail.com>
2023-04-18 19:06:58 -0700

    installed inquirer

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your repository?'
+},
+{
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please type a short description of you project.',
+
+},
+{
+    typut: 'input',
+    name: 'installation',
+    message:
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt (questions)
+.then((response) => {
+    console.log(response);
+    writeToFile("READMES.md", response)
+})
+
+function writeToFile(fileName, data) {
+    const finalPage = `# ${data.title}
+    ## Description
+    ${data.description}
+    
+    ##Table of Content
+    
+    ##Installation
+    
+    ##Usage
+    
+    ##Contributing
+    
+    ##Tests
+    
+    ##License
+    
+    ##Questions
+    GitHub: ${data-username}
+   ${data-contactinfo} `
+}
+
+fs.writeFile(fileName, finalPage, function(error) {
+
+})
 
 // TODO: Create a function to initialize app
 function init() {}
